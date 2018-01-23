74e42a627ec29d603d4e2eacba149bf2296d8997
rfrontiere <remi.frontiere@gmail.com>
2018-01-23 16:23:01 +0100

    23/01/2018

---
+++ app/src/main/java/frontiere/com/outerspacemanager/outerspacemanager/Manager.java
@@ -0,0 +1,35 @@
+package frontiere.com.outerspacemanager.outerspacemanager;
+
+/**
+ * Created by rfrontiere on 23/01/2018.
+ */
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public interface Manager {
+    @POST("auth/create")
+    Call<Token> user(@Body User user);
+
+    @POST("auth/login")
+    Call<Token> login(@Body User user);
+
+    @GET("buildings/list")
+    Call<Buildings> building(@Header("x-access-token") String token);
+
+    @GET("users/:from/:limit")
+    Call<Users> users(@Header("x-access-token") String token);
+
+    @GET("ships")
+    Call<Ships> ships(@Header("x-access-token") String token);
+
+    @POST("buildings/create/{buildingId}")
+    Call<Buildings> createbuilding(@Header("x-access-token") String token, @Path("buildingId") String buildingId);
+
+
+}
---
+++ app/src/main/java/frontiere/com/outerspacemanager/outerspacemanager/User.java
@@ -0,0 +1,62 @@
+package frontiere.com.outerspacemanager.outerspacemanager;
+
+/**
+ * Created by rfrontiere on 23/01/2018.
+ */
+
+
+class User {
+    private String email;
+    private String username;
+    private String password;
+    private Integer score;
+
+    public User(String username, Integer score) {
+        this.username = username;
+        this.score = score;
+    }
+
+    public User(String username, String password, String email) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+}
